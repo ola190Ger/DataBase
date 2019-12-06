05e33c04708d92bd817eb70c91f32efc0c85a2e4
ola190Ger <49449139+ola190Ger@users.noreply.github.com>
2019-12-06 23:46:51 +0200

    Complited
    Курс:  Теория баз данных Тема: Запросы SELECT, INSERT, UPDATE, DELETE. Запросы 1. Вывести таблицу кафедр, но расположить ее поля в обратном порядке. 2. Вывести названия групп и их рейтинги, используя в качестве названий выводимых полей “Group Name” и “Group Rating” соответственно. 3. Вывести для преподавателей их фамилию, процент ставки по отношению к надбавке и процент ставки по отношению к зарплате (сумма ставки и надбавки). 4. Вывести таблицу факультетов в виде одного поля в следующем формате: “The dean of faculty [faculty] is [dean].”. 5. Вывести фамилии преподавателей, которые являются профессорами и ставка которых превышает 1050. 6. Вывести названия кафедр, фонд финансирования которых меньше 11000 или больше 25000. 7. Вывести названия факультетов кроме факультета “Computer Science”. 8. Вывести фамилии и должности преподавателей, которые не являются профессорами. 9. Вывести фамилии, должности, ставки и надбавки ассистентов, у которых надбавка в диапазоне от 160 до 550. 10. Вывести фамилии и ставки ассистентов. 11. Вывести фамилии и должности преподавателей, которые были приняты на работу до 01.01.2000.
    12. Вывести названия кафедр, которые в алфавитном порядке располагаются до кафедры “Software Development”. Выводимое поле должно иметь название “Name of De part ment”. 13. Вывести фамилии ассистентов, имеющих зарплату (сумма ставки и надбавки) не более 1200. 14. Вывести названия групп 5-го курса, имеющих рейтинг в диапазоне от 2 до 4. 15. Вывести фамилии ассистентов со ставкой меньше 550 или надбавкой меньше 200.

--- DB_DZ3.sql
+++ DB_DZ3.sql
@@ -1,4 +1,4 @@
-﻿Create database DZ32;
+Create database DZ32;
 
 use DZ32;
 
@@ -21,8 +21,8 @@ Name nvarchar(100) not null unique check(Name!=''),
 Create table Groups(
 Id int identity not null primary key,
 Name nvarchar(10) not null check(Name!='') unique,
-Rating int not null check(Rating >1 and Rating<5),
-Year int not null check(Year >1 and Year<5),
+Rating int not null check(Rating >=1 and Rating<=5),
+Year int not null check(Year >=1 and Year<=5),
 )
 
 Create table Teacher (
@@ -41,9 +41,9 @@ drop table Teacher
 --заполнение сApex---------------------------------------------
 ---------------------------------------------------------------
 use DZ32;
- --1.Вывести таблицу кафедр, но расположить ее поля в обратном порядке. 
+ --Вывести таблицу кафедр, но расположить ее поля в обратном порядке. 
  select * from Departments order by Id desc
- --2.Вывести названия групп и их рейтинги, используя в качестве названий выводимых полей “Group Name” и “Group Rating” соответственно. 
+ --Вывести названия групп и их рейтинги, используя в качестве названий выводимых полей “Group Name” и “Group Rating” соответственно. 
  select Name as 'Group Name', Rating as 'Group Rating' from Groups 
 --3.Вывести для преподавателей их фамилию, процент ставки по отношению к надбавке и процент ставки по отношению к зарплате (сумма ставки и надбавки). 
  select Surname as 'Surname',
@@ -51,3 +51,30 @@ use DZ32;
 		Salary/(Salary+Premium)*100 as 'Percent 2nd'
 	from Teacher
 --4.Вывести таблицу факультетов в виде одного поля в следующем формате: “The dean of faculty [faculty] is [dean].”.
+select '"The dean of faculty '+Name+' is '+Dean+'."' as DeanFaculti from Faculties
+--5.Вывести фамилии преподавателей, которые являются профессорами и ставка которых превышает 1050. 
+Select Surname from Teacher where IsProfessor=1  and Salary>1050000
+--6.Вывести названия кафедр, фонд финансирования которых меньше 11000 или больше 25000. 
+Select Name from Departments where Financing >20000000000000 and Financing<25000000000000
+--7.  Вывести названия факультетов кроме факультета “R”. 
+Select Name from Departments where Name!='R'
+--8. Вывести фамилии и должности преподавателей, которые не являются профессорами.
+Select Name, Position from Teacher where IsProfessor!=1
+--9.Вывести фамилии, должности, ставки и надбавки ассистентов, у которых надбавка в диапазоне от 160 до 550.(16000000000000-55000000000000)
+Select Surname, Position, Salary, Premium from Teacher where Premium>16000000000000 and Premium<55000000000000
+--10. Вывести фамилии и ставки ассистентов. 
+Select Surname, Salary from Teacher where IsAssistent=1
+--11.Вывести фамилии и должности преподавателей, которые были приняты на работу до 01.01.2000.
+Select Surname, Position from Teacher where EmploymentDate<'2000-01-01'
+--12.Вывести названия кафедр, которые в алфавитном порядке располагаются до кафедры “PRIPJJTYXTVNE”. Выводимое поле должно иметь название “Name of De part ment”. 
+Select Name as 'Name of De part ment' from Departments
+where Name<'PRIPJJTYXTVNE'
+--13.Вывести фамилии ассистентов, имеющих зарплату (сумма ставки и надбавки) не более 1200.12000000000000
+Select Surname from Teacher where (Salary+Premium)<12000000000000
+--14. Вывести названия групп 5-го (3-го) курса, имеющих рейтинг в диапазоне от 2 до 4.
+Select Name, Year from Groups where Year=3 and Rating>=2 and Rating<=4
+--15. Вывести фамилии ассистентов со ставкой меньше 55000000000000 или надбавкой меньше 20000000000000. 
+Select Surname from Teacher where Salary<55000000000000 or Premium<20000000000000
+
+
+
