a20ce01bd8a200d39b9f1ec637b71da2e03ebfa5
ola190Ger <49449139+ola190Ger@users.noreply.github.com>
2019-12-08 00:21:33 +0200

    Update DZ4. Added ##1-5/11

--- DB_DZ4.sql
+++ DB_DZ4.sql
@@ -63,3 +63,50 @@ LecturedId int not null references Lectures(Id)
 
 --Fulling in tables-----------------------------
 ------------------------------------------------
+
+use DZ4
+--1. Вывести все возможные пары строк преподавателей и групп.
+--what to do?
+
+--2. Вывести названия факультетов, фонд финансирования
+--кафедр которых превышает фонд финансирования факультета.
+
+	select (Select Name from Departments
+	where Faculties.Id=Departments.Id and Faculties.Financing<Departments.Financing 
+	) --way to remove NULL
+	from Faculties
+  
+--3. Вывести фамилии кураторов групп и названия групп, которые они курируют.
+select Id,
+	(select Name+' '+Surname  from Curators
+	where Curators.Id=GroupseCurators.Id) as Curator,
+	(select Name from Groups
+	where Groups.Id=GroupseCurators.Id) as 'Group'
+from GroupseCurators
+
+--4. Вывести имена и фамилии преподавателей, которые читают
+--лекции у группы “P107”."OEH"
+	Select Teachers.Name +' '+ Surname   
+	FROM GroupsLectures 
+	join Groups on GroupsLectures.GroupId=Groups.Id 
+	join Lectures on GroupsLectures.LecturedId=Lectures.Id 
+	join Teachers on Lectures.TeacherId=Teachers.Id
+	where Groups.Name='OEH'
+
+--5. Вывести фамилии преподавателей и названия факультетов
+--на которых они читают лекции.
+  Select Teachers.Surname as SurnameTeacher, Faculties.Name as Facultiet
+  from Teachers 
+  Join Lectures on TeacherId=Teachers.Id 
+  Join GroupsLectures on LecturedId=Lectures.Id
+  join Groups on GroupId=Groups.Id
+  join Departments on DepartmentId=Departments.Id
+  join Faculties on FacultyId=Faculties.Id
+
+
+
+
+
+
+
+
