4ccb592d08e42d17ac3c2f08bc1cb5d94cb4c417
ola190Ger <49449139+ola190Ger@users.noreply.github.com>
2019-12-29 12:05:23 +0200

    Completed

--- DB_DZ6.sql
+++ DB_DZ6.sql
@@ -79,16 +79,6 @@ select Departmens.Building, sum(cast(Departmens.Finansing as decimal)) as Summ f
 group by Departmens.Building
 having sum(cast(Departmens.Finansing as decimal)) > 1496159802654852
 
-
-
-----МОЖЛИВО НЕОБХІДНО ВИВЕСТИ НАЗВУ ФАКУЛЬТЕТУ
-
---select Faculties.Name, Sum(Finansing) as FacultiFinance
---from Faculties
---join Departmens on Faculties.Id=FacultiID
---group by Faculties.Name
---having SUM(Finansing)>100000000000000
-
 --2. Вывести названия групп 5-го курса кафедры “Software
 --Development”, которые имеют более 10 пар в первую неделю.
 
@@ -115,8 +105,6 @@ having sum(Rating)>(select sum(Rating)
 					join Groups on GroupId=Groups.Id
 					join Students on StudentId=Students.Id
 					where Groups.Name like 'AFCQBP')
--- rating 'AFCQBP'=3
--- how make easer===========================================
 
 --4. Вывести фамилии и имена преподавателей, ставка которых
 --выше средней ставки профессоров.
@@ -133,8 +121,8 @@ group by Groups.Name
 having count(CuratorId)>1
 
 --6. Вывести названия групп, имеющих рейтинг 
---(средний рейтинг всех студентов группы) меньше, чем минимальный
---рейтинг групп 5-го курса.
+--(средний рейтинг всех студентов группы) меньше, чем минимальный рейтинг групп 5-го курса.
+
 declare @reting5yearsgroup int
 set  @reting5yearsgroup=(select top 1 sum(Rating)
 					from (GroupStudents join Groups on GroupId=Groups.Id
@@ -151,8 +139,39 @@ having sum(Rating)<@reting5yearsgroup
 
 --7. Вывести названия факультетов, суммарный фонд финансирования кафедр которых больше суммарного фонда
 --финансирования кафедр факультета “Computer Science”.
+
+select Faculties.Name from Faculties
+join Departmens on Departmens.FacultiID = Faculties.Id
+group by Faculties.Name
+Having sum(Departmens.Finansing) < (select sum(Departmens.Finansing) from Departmens
+join Faculties on Departmens.FacultiID = Faculties.Id
+Where Faculties.Name = 'F')
+
 --8. Вывести названия дисциплин и полные имена преподавателей, читающих наибольшее количество лекций по ним.
---9. Вывести название дисциплины, по которому читается
---меньше всего лекций.
---10. Вывести количество студентов и читаемых дисциплин на
---кафедре “Software Development”.
+
+select top 10 Subjects.Name as Subjects, Teachers.Surname + '' + Teachers.Name as Teacher
+from Subjects
+join Lectures on Lectures.SubjectId = Subjects.Id
+join Teachers on Teachers.Id = Lectures.TeacherId
+join GroupsLectures on Lectures.Id = GroupsLectures.LecturedId
+group by Subjects.Name, Teachers.Surname , Teachers.Name
+order by COUNT(LecturedId) desc
+
+--9. Вывести название дисциплины, по которому читается меньше всего лекций.
+
+select top 10 Subjects.Name as Subjects
+from Subjects
+join Lectures on Lectures.SubjectId = Subjects.Id
+join GroupsLectures on Lectures.Id = GroupsLectures.LecturedId
+group by Subjects.Name
+order by COUNT(LecturedId)
+
+--10. Вывести количество студентов и читаемых дисциплин на кафедре “Software Development”.
+
+select COUNT(StudentId), Subjects.Name as Subjects from GroupStudents
+join GroupsLectures on GroupsLectures.GroupId = GroupStudents.GroupId
+join Lectures on Lectures.Id = GroupsLectures.LecturedId
+join Subjects on Subjects.Id = Lectures.SubjectId
+join Groups on GroupStudents.GroupId = Groups.Id
+join Departmens on Departmens.Id = Groups.DepartmentId and Departmens.Name like '%G%'
+group by Subjects.Name
